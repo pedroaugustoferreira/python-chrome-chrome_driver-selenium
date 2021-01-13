87308ca852961c22cf40cea5a6abf883d56f4051
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2021-01-12 22:45:45 -0300

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker start 4biz_python_selenium
+sleep 60
+iconv -f iso-8859-1 -t utf-8 $(ls -trh python_selenium/*.csv|grep -v upload|tail -1) | egrep "Em Anda|Suspensa|Reaberto"> python_selenium/upload.csv
+docker exec -i 4biz_db mysql -uroot -padmin chamados < load.sql
