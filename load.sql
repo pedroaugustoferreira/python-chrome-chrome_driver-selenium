185dd98f0cc6d7b8d06c2a0315f6a7af1a58a016
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2021-01-12 23:58:58 -0300

    Update load.sql

--- load.sql
+++ load.sql
@@ -1,5 +1,5 @@
 DELETE FROM torre_unix;
-LOAD DATA INFILE '/var/lib/mysql-files/upload.csv'
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/upload.csv'
 INTO TABLE torre_unix
 FIELDS TERMINATED BY ';'
 IGNORE 1 ROWS;
