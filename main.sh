aa8b90f113595fbb2f6f66a399d4f3492b943527
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2021-01-15 11:33:30 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+cd /chamados/python-chrome-chrome_driver-selenium
+ls *.csv|grep -v upload|awk '{print "rm -rf "$1}'|sh
+
+
 docker start 4biz_python_selenium
 sleep 60
-iconv -f iso-8859-1 -t utf-8 $(ls -trh python_selenium/*.csv|grep -v upload|tail -1) | egrep "Em Anda|Suspensa|Reaberto" | tr -d '"' > python_selenium/upload.csv
+iconv -f iso-8859-1 -t utf-8 $(ls -trh python_selenium/*.csv|grep -v upload|tail -1) | egrep "Em Anda|Suspensa|Reaberta" | tr -d '"'  > python_selenium/upload.csv
 docker exec -i 4biz_db mysql -uroot -padmin chamados < load.sql
