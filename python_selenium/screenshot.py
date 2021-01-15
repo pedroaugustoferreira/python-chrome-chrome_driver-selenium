b9f929cd75973b6c0bd0f291c32a1a4ae599484a
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2021-01-15 11:38:05 -0300

    Create screenshot.py

---
+++ python_selenium/screenshot.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+import os
+import configparser
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage') 
+prefs = {'download.default_directory' : '/app'}
+chrome_options.add_experimental_option('prefs', prefs)
+
+driver = webdriver.Chrome(options=chrome_options, executable_path='/usr/sbin/chromedriver')
+
+# SCREENSHOT
+driver.get("http://191.252.182.151:3000/d/LQWCv3aMz/chamados")
+
+el = driver.find_element_by_name('user')
+el.clear()
+el.send_keys("unix")
+
+el = driver.find_element_by_name('password')
+el.clear()
+el.send_keys("Service@123")
+el.send_keys(Keys.RETURN)
+
+sleep(600)
+
+
+driver.set_window_size(1920, 3000)
+
+el = driver.find_element_by_tag_name('body')
+el.screenshot('/app/img.png')
+driver.quit()
